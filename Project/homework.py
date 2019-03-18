3c6866cc53a5849315ce80be9a0f261a8df9760b
KDK(NoteBook) <rkdqmffor@gmail.com>
2019-03-18 21:21:44 +0900

    HomeWork2 11.1 finish
    숙제 2 11.1까지 완성

--- Project/homework.py
+++ Project/homework.py
@@ -1,10 +1,14 @@
-Set=eval(input("1과 100사이의 정수를 입력하세요: "))
-List=list(Set)
-List.sort()
+def sumColumn(m,columnIndex):
+    global Sum
+    Sum=0
+    for i in columnIndex:
+        Sum+=i[m]
+    return Sum
 
-while(List):
-    print(List[0]," - ",List.count(List[0]),"번 나타납니다.")
-    cnt=List.count(List[0])
-    while(cnt):
-        List.pop(0)
-        cnt-=1
+list1=[float(i) for i in input("3x4 행렬의 행 0 번에 대한 원소를 입력하세요: ").split()]
+list2=[float(i) for i in input("3x4 행렬의 행 1 번에 대한 원소를 입력하세요: ").split()]
+list3=[float(i) for i in input("3x4 행렬의 행 2 번에 대한 원소를 입력하세요: ").split()]
+columnIndex=[list1,list2,list3]
+
+for i in range(0,len(columnIndex)+1):
+    print("열 ",i,"번 원소의 총 합은 ",sumColumn(i,columnIndex))
