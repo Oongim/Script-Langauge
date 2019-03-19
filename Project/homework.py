74d408e6d0687ce94889e86055ca0b77e5a3a50a
Oongim-Desktop <43131702+Oongim@users.noreply.github.com>
2019-03-19 22:53:16 +0900

    homework2 mamori

--- Project/homework.py
+++ Project/homework.py
@@ -1,14 +1,7 @@
-def sumColumn(m,columnIndex):
-    global Sum
-    Sum=0
-    for i in columnIndex:
-        Sum+=i[m]
-    return Sum
+def getRightmostLowestPoint(points):
 
-list1=[float(i) for i in input("3x4 행렬의 행 0 번에 대한 원소를 입력하세요: ").split()]
-list2=[float(i) for i in input("3x4 행렬의 행 1 번에 대한 원소를 입력하세요: ").split()]
-list3=[float(i) for i in input("3x4 행렬의 행 2 번에 대한 원소를 입력하세요: ").split()]
-columnIndex=[list1,list2,list3]
+    return
 
-for i in range(0,len(columnIndex)+1):
-    print("열 ",i,"번 원소의 총 합은 ",sumColumn(i,columnIndex))
+List=[float(x) for x in input("6개의 점을 입력하세요: ").split()]
+
+getRightmostLowestPoint(List)
