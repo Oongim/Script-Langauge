ec86c8c65fde2ea1b3f1286a033d04bde77679cf
Oongim-Desktop <43131702+Oongim@users.noreply.github.com>
2019-03-28 22:28:51 +0900

    tkinter왜 안되는가

--- Project/H4.py
+++ Project/H4.py
@@ -114,4 +114,44 @@
 # S.stop()
 # S.getElapsedTime()
 
+# #12-1
+# class Triangle:
+#     def __init__(self,side1=1.0,side2=1.0,side3=1.0):
+#         self.side1=side1
+#         self.side2=side2
+#         self.side3=side3
+#
+#     def getSide1(self):
+#         return self.side1
+#     def getSide2(self):
+#         return self.side2
+#     def getSide3(self):
+#         return self.side3
+#
+#     def getArea(self):
+#         return
+#     def getPerimeter(self):
+#         return self.side1+self.side2+self.side3
+#
+#     def __str__(self):
+#         return "Triangle: side1 = "+str(self.side1)+"side2 = "+str(self.side2) + "side3 = "+str(self.side3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
